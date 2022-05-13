ca939e42afb59d3f168ade217c3482e920365221
lonlydoge <82283198+lonlydoge@users.noreply.github.com>
2022-05-13 20:14:20 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,87 +1,87 @@
-local NotificationUI = Instance.new("ScreenGui")
-local NotificationFrame = Instance.new("Frame")
-local Notification = Instance.new("Frame")
-local Rounder = Instance.new("UICorner")
-local Image = Instance.new("Frame")
-local RounderAgain = Instance.new("UICorner")
-local ColorThing = Instance.new("UIGradient")
-local InfoImage = Instance.new("ImageLabel")
-local NotificationText = Instance.new("TextLabel")
-local NotificationPositioner = Instance.new("UIListLayout")
-
 if game.CoreGui:FindFirstChild("NotificationUI") then
     game.CoreGui:FindFirstChild("NotificationUI"):Destroy()
 end
 
-local TweenService = game:GetService("TweenService")
-local Debris = game:GetService("Debris")
+	local NotificationUI = Instance.new("ScreenGui")
+local NotificationFrame = Instance.new("Frame")
+local Notification = Instance.new("Frame")
+local Rounder = Instance.new("UICorner")
+local NotificationText = Instance.new("TextLabel")
+local UIGradient = Instance.new("UIGradient")
+local Image = Instance.new("Frame")
+local RounderAgain = Instance.new("UICorner")
+local InfoImage = Instance.new("ImageLabel")
+local UIGradient_2 = Instance.new("UIGradient")
+local NotificationPositioner = Instance.new("UIListLayout")
 
-NotificationUI.Parent = game.CoreGui
+--Properties:
+
 NotificationUI.Name = "NotificationUI"
+NotificationUI.Parent = game.CoreGui
 
 NotificationFrame.Name = "NotificationFrame"
 NotificationFrame.Parent = NotificationUI
 NotificationFrame.BackgroundColor3 = Color3.fromRGB(23, 23, 23)
-NotificationFrame.Size = UDim2.new(0, 343, 0, 491)
-NotificationFrame.Position = UDim2.new(0, 0, .4, 6)
-NotificationFrame.BackgroundTransparency = 1
+NotificationFrame.BackgroundTransparency = 1.000
+NotificationFrame.Position = UDim2.new(0, 0, 1, -490)
+NotificationFrame.Size = UDim2.new(0, 340, 0, 490)
 
 Notification.Name = "Notification"
 Notification.Parent = nil
-Notification.BackgroundColor3 = Color3.fromRGB(33, 33, 33)
-Notification.BackgroundTransparency = 1
-Notification.Position = UDim2.new(0.0524781346, 0, 0.812627316, 0)
-Notification.Size = UDim2.new(0, 307, 0, 37)
+Notification.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Notification.BackgroundTransparency = 1.000
+Notification.Position = UDim2.new(0, 0, 1, -35)
+Notification.Size = UDim2.new(0.897058845, 0, 0.0714285746, 0)
 
 Rounder.CornerRadius = UDim.new(0, 4)
 Rounder.Name = "Rounder"
 Rounder.Parent = Notification
 
+NotificationText.Name = "NotificationText"
+NotificationText.Parent = Notification
+NotificationText.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+NotificationText.BackgroundTransparency = 1.000
+NotificationText.Position = UDim2.new(0.150000006, 0, 0.300000012, 0)
+NotificationText.Size = UDim2.new(0, 204, 0, 15)
+NotificationText.Font = Enum.Font.Gotham
+NotificationText.Text = "Hello"
+NotificationText.TextColor3 = Color3.fromRGB(255, 255, 255)
+NotificationText.TextScaled = true
+NotificationText.TextSize = 14.000
+NotificationText.TextTransparency = 1.000
+NotificationText.TextWrapped = true
+NotificationText.TextXAlignment = Enum.TextXAlignment.Left
+
+UIGradient.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(27, 27, 27)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(36, 36, 36))}
+UIGradient.Parent = Notification
+
 Image.Name = "Image"
 Image.Parent = Notification
-Image.BackgroundTransparency = 1
 Image.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Image.Size = UDim2.new(0, 42, 0, 37)
+Image.BackgroundTransparency = 1.000
+Image.Size = UDim2.new(0, 40, 0, 35)
 
 RounderAgain.CornerRadius = UDim.new(0, 5)
 RounderAgain.Name = "RounderAgain"
 RounderAgain.Parent = Image
 
-ColorThing.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(85, 0, 255)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(115, 0, 255))}
-ColorThing.Name = "ColorThing"
-ColorThing.Parent = Image
-
 InfoImage.Name = "InfoImage"
 InfoImage.Parent = Image
 InfoImage.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 InfoImage.BackgroundTransparency = 1.000
-InfoImage.Position = UDim2.new(0.142857149, 0, 0.0810811222, 0)
+InfoImage.Position = UDim2.new(0.140000001, 0, 0.0799999982, 0)
 InfoImage.Size = UDim2.new(0, 30, 0, 30)
 InfoImage.Image = "http://www.roblox.com/asset/?id=6031071057"
-InfoImage.ImageTransparency = 1
+InfoImage.ImageTransparency = 1.000
 
-NotificationText.Name = "NotificationText"
-NotificationText.Parent = Notification
-NotificationText.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-NotificationText.BackgroundTransparency = 1.000
-NotificationText.Position = UDim2.new(0.159609124, 0, 0.297297299, 0)
-NotificationText.Size = UDim2.new(0, 204, 0, 15)
-NotificationText.Font = Enum.Font.Gotham
-NotificationText.Text = ""
-NotificationText.TextColor3 = Color3.fromRGB(255, 255, 255)
-NotificationText.TextScaled = true
-NotificationText.TextSize = 14.000
-NotificationText.TextWrapped = true
-NotificationText.TextTransparency = 1
-NotificationText.TextXAlignment = Enum.TextXAlignment.Left
+UIGradient_2.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(27, 27, 27)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(36, 36, 36))}
+UIGradient_2.Parent = Image
 
 NotificationPositioner.Name = "NotificationPositioner"
 NotificationPositioner.Parent = NotificationFrame
 NotificationPositioner.SortOrder = Enum.SortOrder.LayoutOrder
 NotificationPositioner.VerticalAlignment = Enum.VerticalAlignment.Bottom
 NotificationPositioner.Padding = UDim.new(0, 5)
-
--- UI Done
 
 local System = {}
 
@@ -137,5 +137,3 @@ function System.Notify(Text, Duration)
         Debris:AddItem(clonedNotification, .5)
     end)
 end
-
-return System
