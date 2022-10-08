99f12688ff164895c2feca09dada8861232e1f89
lonlydoge <82283198+lonlydoge@users.noreply.github.com>
2022-10-08 19:42:59 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -14,11 +14,8 @@ end
 local System = {}
 
 local TransparencyValues = {
-    ["UIStroke"] = "Transparency",
-    ["Accept"] = "TextTransparency",
     ["Description"] = "TextTransparency",
     ["Title"] = "TextTransparency",
-    ["Icon"] = "ImageTransparency",
 }
 
 local LocalStorage = Instance.new("Folder", ReplicatedStorage);
@@ -27,20 +24,12 @@ LocalStorage.Name = "LocalStorage";
 local UI = InsertService:LoadLocalAsset("rbxassetid://9637478938"):Clone();
 
 local Notification = UI.NotificationFrame.Notification;
-local BooleanNotification = UI.NotificationFrame.BooleanNotification;
 
 Notification.Parent = LocalStorage
-BooleanNotification.Parent = LocalStorage
 
-for _, v in ipairs(Notification:GetDescendants()) do
-    if TransparencyValues[v.Name] then
-        v[TransparencyValues[v.Name]] = 1;
-    end
-end
-
-for _, v in ipairs(BooleanNotification:GetDescendants()) do
-    if TransparencyValues[v.Name] then
-        v[TransparencyValues[v.Name]] = 1;
+for _, Object in ipairs(Notification:GetDescendants()) do
+    if TransparencyValues[Object.Name] then
+        Object[TransparencyValues[Object.Name]] = 1;
     end
 end
 
@@ -60,10 +49,10 @@ System.Notify = function(Title, Text, Duration)
 
     FrameTween:Play();
 
-    for _, v in pairs(ClonedNotification:GetDescendants()) do
-        if TransparencyValues[v.Name] then
-            local Tween = TweenService:Create(v, TweenInfo.new(0.5), {
-                [TransparencyValues[v.Name]] = 0;
+    for _, Object in pairs(ClonedNotification:GetDescendants()) do
+        if TransparencyValues[Object.Name] then
+            local Tween = TweenService:Create(Object, TweenInfo.new(0.5), {
+                [TransparencyValues[Object.Name]] = 0;
             });
         
             Tween:Play();
@@ -77,10 +66,10 @@ System.Notify = function(Title, Text, Duration)
 
         FrameTween:Play();
 
-        for _, v in pairs(ClonedNotification:GetDescendants()) do
-            if TransparencyValues[v.Name] then
-                local Tween = TweenService:Create(v, TweenInfo.new(0.5), {
-                    [TransparencyValues[v.Name]] = 1;
+        for _, Object in pairs(ClonedNotification:GetDescendants()) do
+            if TransparencyValues[Object.Name] then
+                local Tween = TweenService:Create(Object, TweenInfo.new(0.5), {
+                    [TransparencyValues[Object.Name]] = 1;
                 });
             
                 Tween:Play();
@@ -91,89 +80,6 @@ System.Notify = function(Title, Text, Duration)
             ClonedNotification:Destroy();
         end);
     end);
-end
-
-System.BooleanNotify = function(Title, Text, Duration)
-    local Pressed = false;
-    local PassedTime = tick();
-
-    local ClonedNotification = BooleanNotification:Clone();
-
-    ClonedNotification.Parent = UI.NotificationFrame;
-
-    ClonedNotification.Title.Text = Title;
-    ClonedNotification.Description.Text = Text;
-
-    local FrameTween = TweenService:Create(ClonedNotification, TweenInfo.new(0.5), {
-        BackgroundTransparency = 0;
-    });
-
-    FrameTween:Play();
-
-    for _, v in pairs(ClonedNotification:GetDescendants()) do
-        if TransparencyValues[v.Name] then
-            local Tween = TweenService:Create(v, TweenInfo.new(0.5), {
-                [TransparencyValues[v.Name]] = 0;
-            });
-        
-            Tween:Play();
-        end
-    end
-
-    local Keybind = UIS.InputBegan:Connect(function(input, gameProcessedEvent)
-        if input.KeyCode == Enum.KeyCode.Y and not gameProcessedEvent then
-            local FrameTween = TweenService:Create(ClonedNotification, TweenInfo.new(0.5), {
-                BackgroundTransparency = 1;
-            });
-    
-            FrameTween:Play();
-    
-            for _, v in pairs(ClonedNotification:GetDescendants()) do
-                if TransparencyValues[v.Name] then
-                    local Tween = TweenService:Create(v, TweenInfo.new(0.5), {
-                        [TransparencyValues[v.Name]] = 1;
-                    });
-                
-                    Tween:Play();
-                end
-            end
-    
-            task.delay(0.5, function()
-                ClonedNotification:Destroy();
-            end);
-
-            Pressed = true;
-            return
-        end
-    end)
-
-    repeat game:GetService("RunService").RenderStepped:wait() until math.round(tick() - PassedTime, 2) >= Duration or Pressed;
-
-    if Pressed then
-        return true;
-    end
-
-    local FrameTween = TweenService:Create(ClonedNotification, TweenInfo.new(0.5), {
-        BackgroundTransparency = 1;
-    });
-
-    FrameTween:Play();
-
-    for _, v in pairs(ClonedNotification:GetDescendants()) do
-        if TransparencyValues[v.Name] then
-            local Tween = TweenService:Create(v, TweenInfo.new(0.5), {
-                [TransparencyValues[v.Name]] = 1;
-            });
-        
-            Tween:Play();
-        end
-    end
-
-    task.delay(0.5, function()
-        ClonedNotification:Destroy();
-    end);
-
-    return false
 end
 
 return System;
